1eb66333550e1dac60bcfa1f2263feeb5e855f8d
Shahriar118 <shahriarmahmud@iut-dhaka.edu>
2023-06-11 16:13:05 +0600

    tv shows page done

--- lib/tv_shows.dart
+++ lib/tv_shows.dart
@@ -13,12 +13,18 @@ class TVShowsPage extends StatefulWidget {
 class _TVShowsPageState extends State<TVShowsPage> {
   List<dynamic> trendingTVShows = [];
   List<dynamic> airingTodayTVShows = [];
+  List<dynamic> onTVTVShows = [];
+  List<dynamic> popularTVShows = [];
+  List<dynamic> topRatedTVShows = [];
 
   @override
   void initState() {
     super.initState();
     fetchTrendingTVShows();
     fetchAiringTodayTVShows();
+    fetchOnTVTVShows();
+    fetchPopularTVShows();
+    fetchTopRatedTVShows();
   }
 
   Future<void> fetchTrendingTVShows() async {
@@ -53,6 +59,54 @@ class _TVShowsPageState extends State<TVShowsPage> {
     }
   }
 
+  Future<void> fetchOnTVTVShows() async {
+    final apiKey = '080184f9aad4105504265a00cf70d578';
+    final url = 'https://api.themoviedb.org/3/tv/on_the_air?api_key=$apiKey';
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final jsonResponse = json.decode(response.body);
+      setState(() {
+        onTVTVShows = jsonResponse['results'];
+      });
+    } else {
+      throw Exception('Failed to load "On TV" TV shows');
+    }
+  }
+
+  Future<void> fetchPopularTVShows() async {
+    final apiKey = '080184f9aad4105504265a00cf70d578';
+    final url = 'https://api.themoviedb.org/3/tv/popular?api_key=$apiKey';
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final jsonResponse = json.decode(response.body);
+      setState(() {
+        popularTVShows = jsonResponse['results'];
+      });
+    } else {
+      throw Exception('Failed to load popular TV shows');
+    }
+  }
+
+  Future<void> fetchTopRatedTVShows() async {
+    final apiKey = '080184f9aad4105504265a00cf70d578';
+    final url = 'https://api.themoviedb.org/3/tv/top_rated?api_key=$apiKey';
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final jsonResponse = json.decode(response.body);
+      setState(() {
+        topRatedTVShows = jsonResponse['results'];
+      });
+    } else {
+      throw Exception('Failed to load top-rated TV shows');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,7 +114,6 @@ class _TVShowsPageState extends State<TVShowsPage> {
         title: Text('TV Shows'),
       ),
       body: SingleChildScrollView(
-        physics: AlwaysScrollableScrollPhysics(),
         child: Column(
           children: <Widget>[
             Padding(
@@ -115,6 +168,117 @@ class _TVShowsPageState extends State<TVShowsPage> {
               child: Row(
                 children: <Widget>[
                   for (var show in airingTodayTVShows)
+                    GestureDetector(
+                      onTap: () {
+                        // Handle TV show item tap
+                      },
+                      child: Container(
+                        width: 150.0,
+                        height: 200.0,
+                        margin: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8.0),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                              'https://image.tmdb.org/t/p/w200${show['poster_path']}',
+                            ),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'On TV',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: <Widget>[
+                  for (var show in onTVTVShows)
+                    GestureDetector(
+                      onTap: () {
+                        // Handle TV show item tap
+                      },
+                      child: Container(
+                        width: 150.0,
+                        height: 200.0,
+                        margin: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8.0),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                              'https://image.tmdb.org/t/p/w200${show['poster_path']}',
+                            ),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Popular',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: <Widget>[
+                  for (var show in popularTVShows)
+                    GestureDetector(
+                      onTap: () {
+                        // Handle TV show item tap
+                      },
+                      child: Container(
+                        width: 150.0,
+                        height: 200.0,
+                        margin: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8.0),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                              'https://image.tmdb.org/t/p/w200${show['poster_path']}',
+                            ),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Top Rated',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: <Widget>[
+                  for (var show in topRatedTVShows)
                     GestureDetector(
                       onTap: () {
                         // Handle TV show item tap
